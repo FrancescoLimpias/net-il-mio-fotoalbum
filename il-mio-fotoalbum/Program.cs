318ff76c0f61f4cabe8e41d9f147dcc9f39785f0
Francesco Limpias <francescolimpias@gmail.com>
2023-05-16 17:15:14 +0200

    Improved Database Seeding - added roles and users seeding - minor improvements

--- il-mio-fotoalbum/Program.cs
+++ il-mio-fotoalbum/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using il_mio_fotoalbum.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 namespace il_mio_fotoalbum
 {
     public class Program
@@ -39,6 +41,8 @@ namespace il_mio_fotoalbum
 
             app.UseAuthorization();
 
+            CreateRolesandUsers(app);
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
@@ -46,6 +50,47 @@ namespace il_mio_fotoalbum
             app.MapRazorPages();
 
             app.Run();
+        }
+
+        private static async void CreateRolesandUsers(WebApplication app)
+        {
+
+            var ServiceProvider = app.Services.CreateScope().ServiceProvider;
+
+            var RoleManager = ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            bool condition = await RoleManager.RoleExistsAsync("ADMIN");
+            if (!condition)
+            {
+                // create admin creatorRole   
+                var adminRole = new IdentityRole();
+                adminRole.Name = "ADMIN";
+                await RoleManager.CreateAsync(adminRole);
+
+                // generate my account      
+                var user = new IdentityUser();
+
+                user.UserName = "francescolimpias@gmail.com";
+                user.Email = "francescolimpias@gmail.com";
+                user.EmailConfirmed = true;
+                string userPass = "code9CODE*";
+
+                IdentityResult checkUser = await UserManager.CreateAsync(user, userPass);
+
+                //Add default User to Role Admin    
+                if (checkUser.Succeeded)
+                    await UserManager.AddToRoleAsync(user, "ADMIN");
+            }
+
+            // creating Creating Manager adminRole     
+            condition = await RoleManager.RoleExistsAsync("CREATOR");
+            if (!condition)
+            {
+                var creatorRole = new IdentityRole();
+                creatorRole.Name = "CREATOR";
+                await RoleManager.CreateAsync(creatorRole);
+            }
         }
     }
 }
